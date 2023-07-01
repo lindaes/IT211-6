a8ab9c700221a589f77e6addd38d827f2eac7d6b
Replit user <>
2023-07-01 23:32:20 +0000

    changes

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+var firstName = "Linda";
+var lastName = "Eschete";
+var yearsOfStudy = 2;
+var goal = "finish my education and recieve my bachelor's after completeing Year Up";
 
+console.log("My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " years and plan to " + goal + " after completing my courses.");
